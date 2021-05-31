0d2cb1bde843de3c137a63e04c7916c944f4597f
Mayank Vishwakarma <mayankat03@gmail.com>
2021-05-31 16:23:38 +0530

    hospital updates

--- scripts/script2.js
+++ scripts/script2.js
@@ -5,12 +5,12 @@
 //             var table = document.getElementById('mphospitalinfo')
 //             for (var i = 0; i < data.length; i++){
 //                 var row = `<tr>
-// 						      <td class="text-dark text-semibold ">${data[i].district}</td>
-//                             <td class="text-dark text-semibold ">${data[i].name}</td>
-//                             <td class="text-dark text-semibold ">${data[i].type}</td>
-//                             <td class="text-dark text-semibold ">${data[i].contact1}</td>
-//                             <td class="text-dark text-semibold ">${data[i].contact2}</td>
-//                             <td class="text-dark text-semibold ">${data[i].contact3}</td>
+// 						      <td class="text-dark text-semibold " style="text-align: center; vertical-align: middle;">${data[i].district}</td>
+//                             <td class="text-dark text-semibold " style="vertical-align: middle;">${data[i].name}</td>
+//                             <td class="text-dark text-semibold " style="text-align: center; vertical-align: middle;">${data[i].type}</td>
+//                             <td class="text-dark text-semibold " style="text-align: center; vertical-align: middle;">${data[i].contact1[0]} <br><a href="tel:${data[i].contact1[1]}">${data[i].contact1[1]}</a></td>
+//                             <td class="text-dark text-semibold " style="text-align: center; vertical-align: middle;">${data[i].contact2[0]} <br><a href="tel:${data[i].contact2[1]}">${data[i].contact2[1]}</a></td>
+//                             <td class="text-dark text-semibold " style="text-align: center; vertical-align: middle;">${data[i].contact3[0]} <br><a href="tel:${data[i].contact3[1]}">${data[i].contact3[1]}</a></td>
 //                         </tr>`
 //                 table.innerHTML += row
 //             }
