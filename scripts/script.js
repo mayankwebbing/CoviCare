5d912367ee80eca132700144b51e644d8c7dbf84
Mayank Vishwakarma <mayankat03@gmail.com>
2021-07-04 20:53:55 +0530

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -124,96 +124,96 @@ $.getJSON("https://api.rootnet.in/covid19-in/stats/latest", function(latest){
     $(".totalDeaths").append(total.summary.deaths);
 })
 
-// $.getJSON("https://api.rootnet.in/covid19-in/stats/latest", function(latestData){
+$.getJSON("https://api.rootnet.in/covid19-in/stats/latest", function(latestData){
             
-//             var re = latestData.data.regional;
-//             buildTable(re)
-//             function buildTable(data){
-// 		        var table = document.getElementById('myStateTable')
-//                 for (var i = 0; i < data.length; i++){
-//                     var aa = data[i].totalConfirmed - data[i].discharged - data[i].deaths;
-//                     var row = `<tr>
-// 						        <td class="text-dark text-semibold ">${data[i].loc}</td>
-//                                 <td class="text-danger text-semibold ">${data[i].totalConfirmed}</td>
-//                                 <td class="text-success text-semibold ">${data[i].discharged}</td>
-//                                 <td class="text-info text-semibold ">${aa}</td>
-//                                 <td class="text-secondary text-semibold ">${data[i].deaths}</td>
-//                             </tr>`
-// 			        table.innerHTML += row
-// 		        }
-//             }
-// })
+            var re = latestData.data.regional;
+            buildTable(re)
+            function buildTable(data){
+		        var table = document.getElementById('myStateTable')
+                for (var i = 0; i < data.length; i++){
+                    var aa = data[i].totalConfirmed - data[i].discharged - data[i].deaths;
+                    var row = `<tr>
+						        <td class="text-dark text-semibold ">${data[i].loc}</td>
+                                <td class="text-danger text-semibold ">${data[i].totalConfirmed}</td>
+                                <td class="text-success text-semibold ">${data[i].discharged}</td>
+                                <td class="text-info text-semibold ">${aa}</td>
+                                <td class="text-secondary text-semibold ">${data[i].deaths}</td>
+                            </tr>`
+			        table.innerHTML += row
+		        }
+            }
+})
 
-// function myTableSearch() {
-//   // Declare variables
-//   var input, filter, table, tr, td, i, txtValue;
-//   input = document.getElementById("myTableInput");
-//   filter = input.value.toUpperCase();
-//   table = document.getElementById("myStateTable");
-//   tr = table.getElementsByTagName("tr");
+function myTableSearch() {
+  // Declare variables
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("myTableInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("myStateTable");
+  tr = table.getElementsByTagName("tr");
 
-//   // Loop through all table rows, and hide those who don't match the search query
-//   for (i = 0; i < tr.length; i++) {
-//     td = tr[i].getElementsByTagName("td")[0];
-//     if (td) {
-//       txtValue = td.textContent || td.innerText;
-//       if (txtValue.toUpperCase().indexOf(filter) > -1) {
-//         tr[i].style.display = "";
-//       } else {
-//         tr[i].style.display = "none";
-//       }
-//     }
-//   }
-// }
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
 
-// function sortTableStr(n) {
-//   var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
-//   table = document.getElementById("myStateTable");
-//   switching = true;
-//   //Set the sorting direction to ascending:
-//   dir = "asc"; 
-//   //Make a loop that will continue until no switching has been done:
-//   while (switching) {
-//     //start by saying: no switching is done:
-//     switching = false;
-//     rows = table.rows;
-//     //Loop through all table rows (except the first, which contains table headers):
-//     for (i = 1; i < (rows.length - 1); i++) {
-//       //start by saying there should be no switching:
-//       shouldSwitch = false;
-//       //Get the two elements you want to compare,      one from current row and one from the next:
-//       x = rows[i].getElementsByTagName("TD")[n];
-//       y = rows[i + 1].getElementsByTagName("TD")[n];
-//       //check if the two rows should switch place,      based on the direction, asc or desc:
-//       if (dir == "asc") {
-//         if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-//           //if so, mark as a switch and break the loop:
-//           shouldSwitch= true;
-//           break;
-//         }
-//       } else if (dir == "desc") {
-//         if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-//           //if so, mark as a switch and break the loop:
-//           shouldSwitch = true;
-//           break;
-//         }
-//       }
-//     }
-//     if (shouldSwitch) {
-//       //if a switch has been marked, make the switch      and mark that a switch has been done:
-//       rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-//       switching = true;
-//       //Each time a switch is done, increase this count by 1:
-//       switchcount ++;      
-//     } else {
-//       //If no switching has been done AND the direction is "asc",      set the direction to "desc" and run the while loop again.
-//       if (switchcount == 0 && dir == "asc") {
-//         dir = "desc";
-//         switching = true;
-//       }
-//     }
-//   }
-// }
+function sortTableStr(n) {
+  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+  table = document.getElementById("myStateTable");
+  switching = true;
+  //Set the sorting direction to ascending:
+  dir = "asc"; 
+  //Make a loop that will continue until no switching has been done:
+  while (switching) {
+    //start by saying: no switching is done:
+    switching = false;
+    rows = table.rows;
+    //Loop through all table rows (except the first, which contains table headers):
+    for (i = 1; i < (rows.length - 1); i++) {
+      //start by saying there should be no switching:
+      shouldSwitch = false;
+      //Get the two elements you want to compare,      one from current row and one from the next:
+      x = rows[i].getElementsByTagName("TD")[n];
+      y = rows[i + 1].getElementsByTagName("TD")[n];
+      //check if the two rows should switch place,      based on the direction, asc or desc:
+      if (dir == "asc") {
+        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+          //if so, mark as a switch and break the loop:
+          shouldSwitch= true;
+          break;
+        }
+      } else if (dir == "desc") {
+        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+          //if so, mark as a switch and break the loop:
+          shouldSwitch = true;
+          break;
+        }
+      }
+    }
+    if (shouldSwitch) {
+      //if a switch has been marked, make the switch      and mark that a switch has been done:
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+      //Each time a switch is done, increase this count by 1:
+      switchcount ++;      
+    } else {
+      //If no switching has been done AND the direction is "asc",      set the direction to "desc" and run the while loop again.
+      if (switchcount == 0 && dir == "asc") {
+        dir = "desc";
+        switching = true;
+      }
+    }
+  }
+}
 
 // function sortTableNo(n) {
 //     var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
