9a4e3ae241e86a654f8f4f721d94946e43b21c89
Mayank Vishwakarma <mayankat03@gmail.com>
2021-07-04 20:43:29 +0530

    updating hospital info page
    added page limit with DataTable

--- scripts/script2.js
+++ scripts/script2.js
@@ -2,7 +2,7 @@
 //         var re = hospitalinfoupdate;
 //         buildTable(re)
 //         function buildTable(data){
-//             var table = document.getElementById('mphospitalinfo')
+//             var table = document.getElementById('myhospitalinfo')
 //             for (var i = 0; i < data.length; i++){
 //                 var row = `<tr>
 // 						      <td class="text-dark text-semibold " style="text-align: center; vertical-align: middle;">${data[i].district}</td>
@@ -17,37 +17,38 @@
 //         }
 // })
 
-function myTableSearch() {
-    // Declare variables
-    var input, filter, table, tr, td, i, txtValue;
-    input = document.getElementById("myTableInput");
-    filter = input.value.toUpperCase();
-    table = document.getElementById("mphospitalinfo");
-    tr = table.getElementsByTagName("tr");
+// function myTableSearch() {
+//     // Declare variables
+//     var input, filter, table, tr, td, i, txtValue;
+//     input = document.getElementById("myTableInput");
+//     filter = input.value.toUpperCase();
+//     table = document.getElementById("myhospitalinfo");
+//     tr = table.getElementsByTagName("tr");
   
-    // Loop through all table rows, and hide those who don't match the search query
-    for (i = 0; i < tr.length; i++) {
-      td = tr[i].getElementsByTagName("td")[0];
-      if (td) {
-        txtValue = td.textContent || td.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-          tr[i].style.display = "";
-        } else {
-          tr[i].style.display = "none";
-        }
-      }
-    }
-}
-
-$('#mphospitalinfo').dataTable( {
-  "pageLength": 10
-} );
-$(document).ready(function() { 
-  $('#example').DataTable( { 
-    dom: 'Bfrtip', 
-    lengthMenu: [ 
-      [ 10, 25, 50, -1 ], 
-      [ '10 rows', '25 rows', '50 rows', 'Show all' ] ], 
-      buttons: [ 'pageLength' ] 
-  } ); 
-} );
+//     // Loop through all table rows, and hide those who don't match the search query
+//     for (i = 0; i < tr.length; i++) {
+//       td = tr[i].getElementsByTagName("td")[0];
+//       if (td) {
+//         txtValue = td.textContent || td.innerText;
+//         if (txtValue.toUpperCase().indexOf(filter) > -1) {
+//           tr[i].style.display = "";
+//         } else {
+//           tr[i].style.display = "none";
+//         }
+//       }
+//     }
+// }
+// $(document).ready(function() { 
+//   $('#myhospitalinfo').dataTable( {
+//     "pageLength": 10
+//   } );
+// } );
+// $(document).ready(function() { 
+//   $('#myhospitalinfo').DataTable( { 
+//     dom: 'Bfrtip', 
+//     lengthMenu: [ 
+//       [ 10, 25, 50, -1 ], 
+//       [ '10 rows', '25 rows', '50 rows', 'Show all' ] ], 
+//       buttons: [ 'pageLength' ] 
+//   } ); 
+// } );
